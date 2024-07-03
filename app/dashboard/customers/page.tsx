6285ba4e14e33c9a926b8ac4076a1abb51c4e288
张正一 <zhangzy@zx.com>
2024-07-03 18:58:48 +0800

    [M] chapter 12

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { useEffect } from "react";
+
+
 export default function Page() {
+  console.log('12312312312sss')
+  useEffect(() => {
+    console.log('12312312312')
+  }, [])
   return <p>Customers Page</p>;
 }
