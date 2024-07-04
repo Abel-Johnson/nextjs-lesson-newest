49c7c6a13ae738d37162852c3df3a761b2f67d72
张正一 <zhangzy@zx.com>
2024-07-04 18:33:54 +0800

    [M] chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,12 @@
 
 import { useEffect } from "react";
 
+import { Metadata } from 'next';
 
+
+export const metadata: Metadata = {
+  title: 'customers',
+};
 export default function Page() {
   console.log('12312312312sss')
   useEffect(() => {
